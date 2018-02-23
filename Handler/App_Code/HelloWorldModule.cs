3f3d0e97d8d167c3477c388c5848ca7d5fe9874b
tjcertified <tjcertified@gmail.com>
2018-02-22 22:08:03 -0600

    real initial commit

---
+++ Handler/App_Code/HelloWorldModule.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for HelloWorldModule
+/// </summary>
+public class HelloWorldModule : IHttpModule
+{
+    private List<long> contentSizes;
+
+    public HelloWorldModule()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+        contentSizes = new List<long>();
+    }
+
+    public String ModuleName
+    {
+        get { return "HelloWorldModule"; }
+    }
+
+    public void Dispose()
+    {
+    }
+
+    public void Init(HttpApplication application)
+    {         
+        application.BeginRequest += Application_BeginRequest;
+        application.EndRequest += Application_EndRequest;
+        application.PreRequestHandlerExecute += Application_PreRequestHandlerExecute;
+        application.PostRequestHandlerExecute += Application_PostRequestHandlerExecute;
+    }
+
+    private void Application_PreRequestHandlerExecute(object sender, EventArgs e)
+    {
+        HttpContext.Current.Items[HANDLER_START_KEY] = DateTime.Now;
+    }
+
+    private void Application_PostRequestHandlerExecute(object sender, EventArgs e)
+    {
+        HttpContext.Current.Items[HANDLER_END_KEY] = DateTime.Now;
+    }
+
+    private void Application_EndRequest(object sender, EventArgs e)
+    {
+        // This means that the request did not go completely through the pipeline for some reason
+        // (css file, redirect, image, etc.), so it is not necessarily going to have all of the
+        // information that we need to process it.
+        if (HttpContext.Current.Items[HANDLER_END_KEY] == null)
+        {
+            return;
+        }
+        var appEnd = DateTime.Now;
+        DateTime appStart = (DateTime)HttpContext.Current.Items[APP_START_KEY];
+        DateTime handlerStart = (DateTime)HttpContext.Current.Items[HANDLER_START_KEY];
+        DateTime handlerEnd = (DateTime)HttpContext.Current.Items[HANDLER_END_KEY];
+        contentSizes.Add(_watcher.Length);
+        HttpContext.Current.Response.Write("<hr><h1><font color=red> HelloWorldModule: Begin Request </font></h1>"
+            + "<p>Response Size: " + _watcher.Length + " bytes.</p>"
+            + "<p>Total Pipeline Requests So Far: " + contentSizes.Count + "</p>"
+            + "<p>Total Request Time: " + appEnd.Subtract(appStart).TotalMilliseconds + "ms.</p>"
+            + "<p>Total HttpHandler Time: " + handlerEnd.Subtract(handlerStart).TotalMilliseconds + "ms.</p>"
+            + "<p>Average response size: " + contentSizes.Average() + " bytes.</p>"
+            + "<p>Largest response size: " + contentSizes.Max() + " bytes.</p>"
+            + "<p>Smallest response size: " + contentSizes.Min() + " bytes.</p>"
+            + "<hr>");
+    }
+
+    private void Application_BeginRequest(object sender, EventArgs e)
+    {
+        HttpContext.Current.Items[APP_START_KEY] = DateTime.Now;
+        _watcher = new StreamWatcher(HttpContext.Current.Response.Filter);
+        HttpContext.Current.Response.Filter = _watcher;
+    }
+
+    private const string APP_START_KEY = "appStartTime";
+    private const string APP_END_KEY = "appEndTime";
+    private const string HANDLER_START_KEY = "handlerStartTime";
+    private const string HANDLER_END_KEY = "handlerEndTime";
+    private StreamWatcher _watcher;
+}
---
+++ Handler/App_Code/StreamWatcher.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+/// <summary>
+/// Summary description for StreamWatcher
+/// </summary>
+public class StreamWatcher : Stream
+{
+    private Stream _base;
+    private MemoryStream _memoryStream = new MemoryStream();
+
+    public StreamWatcher(Stream stream)
+    {
+        _base = stream;
+    }
+
+    public override void Flush()
+    {
+        _base.Flush();
+    }
+
+    public override int Read(byte[] buffer, int offset, int count)
+    {
+        return _base.Read(buffer, offset, count);
+    }
+
+    public override void Write(byte[] buffer, int offset, int count)
+    {
+        _memoryStream.Write(buffer, offset, count);
+        _base.Write(buffer, offset, count);
+    }
+
+    public override string ToString()
+    {
+        return Encoding.UTF8.GetString(_memoryStream.ToArray());
+    }
+
+    #region Rest of the overrides
+    public override bool CanRead
+    {
+        get { return _memoryStream.CanRead; }
+    }
+
+    public override bool CanSeek
+    {
+        get { return _memoryStream.CanSeek; }
+    }
+
+    public override bool CanWrite
+    {
+        get { return _memoryStream.CanWrite; }
+    }
+
+    public override long Seek(long offset, SeekOrigin origin)
+    {
+        return _memoryStream.Seek(offset, origin);
+    }
+
+    public override void SetLength(long value)
+    {
+        _memoryStream.SetLength(value);
+    }
+
+    public override long Length
+    {
+        get { return _memoryStream.Length; }
+    }
+
+    public override long Position
+    {
+        get
+        {
+            return _memoryStream.Position;
+        }
+        set
+        {
+            _memoryStream.Position = value;
+        }
+    }
+    #endregion
+}
